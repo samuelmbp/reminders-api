633690133dbf448b0936f9a57088dcc2626f4382
Samuel Raducan <samuel.thembp@gmail.com>
2022-08-08 10:34:04 +0100

    Create a get router for the list of reminders

--- index.ts
+++ index.ts
@@ -1,4 +1,13 @@
 import express from 'express';
+import remindersRouter from './routers/reminders'
 
 const app = express();
+
+app.use('/reminders', remindersRouter);
+
+app.get('/', (req, res) => {
+    res.send('Hello, world.');
+})
+
+
 app.listen(8000, () => console.log('Server started.'));
